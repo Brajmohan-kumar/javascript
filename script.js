f69b8837a68e15dd3ced9e7a28cc88d2477a00e2
brajmohan <brajmohan96390@gmail.com>
2023-11-22 23:38:53 +0530

    JavaScript Variables

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hey this is JavaScript")
+
+var a = 5;
+var b = 7;
+var c = "Braj";
+var _a = "Shubham"
+
+console.log(a + b + 8 )
+console.log(typeof a, typeof b, typeof c)
+
+// const a1 = 6;
+// a1 = a1 + 1;
+
+let x = "Mohan";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, r)
+console.log(typeof x, typeof y, typeof z, typeof q, typeof p, typeof r)
+
+let o = {
+    "name": "Mohan",
+    "job code": 5600
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
